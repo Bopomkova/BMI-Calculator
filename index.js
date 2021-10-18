36b6ae66937c393a75aed6758c5d033cd877e623
Bopomkova <bjowens06@gmail.com>
2021-10-18 13:12:42 -0400

    10/18, variables

---
+++ index.js
@@ -0,0 +1,8 @@
+let variable;
+variable = "Variable";
+let number = 7;
+let string = "Hello";
+let bigString = "Hello but big";
+console.log("variable");
+const constant = "Constant";
+let add = 7 + 7;
